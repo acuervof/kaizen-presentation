d077f389195c4b013de7af99bc451ef8f28418e1
acuervof <acuervof@gmail.com>
2016-04-07 23:04:20 +0200

    copy-example commit

---
+++ copy-example.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let aws = require('aws-sdk');
+let s3 = new aws.S3();
+
+exports.handler = (event, context, callback) => {
+
+    // Get the object from the event and show its content type
+    const srcBucket = event.Records[0].s3.bucket.name;
+    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    
+    const dstBucket = 'id.kaizen.dest';
+    const dstKey = 'copy-' + srcKey;
+    
+    s3.copyObject({
+        Bucket: dstBucket,
+        Key: dstKey,
+        CopySource: srcBucket +'/'+ srcKey
+    }, function(err, data) {
+      if (err) {
+          console.log(err);
+          callback(err);
+      } else {
+          callback();
+      }
+    });
+};
